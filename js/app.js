3034da8dae222c9f60bbed5b2f0acb68b583a283
Aleksei Ogoltcov <aaogoltcov@mail.ru>
2020-09-26 12:42:49 +0300

    init commit

---
+++ js/app.js
@@ -0,0 +1,4 @@
+import '../src/css/style.css'
+import './rainbows.js';
+import './unicorns.js';
+console.log('app worked');
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: "./js/app.js",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+        new HtmlWebPackPlugin()
+    ]
+};
